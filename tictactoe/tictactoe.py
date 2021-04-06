5547506c7449bd3248954ccb17d3153b4fc7e982
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2021-04-06 21:25:14 +0300

    Tic-Tac_Toe Stage 5/5
    Tic-Tac_Toe Python project. Stage 5/5 complete

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,9 +1,11 @@
-def init_cells(source_line):
-    if len(source_line) < 9:
-        print("Error! Length < 9!")
-    else:
-        result = [[j for j in source_line[index * 3:index * 3 + 3]] for index in range(3)]
-        return result
+empty_symbol = '_'
+player1 = 'X'
+player2 = 'O'
+
+
+def init_cells(symbol):
+    result = [[symbol, symbol, symbol], [symbol, symbol, symbol], [symbol, symbol, symbol]]
+    return result
 
 
 def print_field(source_field):
@@ -29,45 +31,39 @@ def check_diag_win(symbol, source_field):
 
 def check_col_row_win(symbol, source_field):
     for row in range(3):
-        rows = True
-        columns = True
+        rows_win = True
+        columns_win = True
 
         for col in range(3):
-            rows &= source_field[row][col] == symbol
-            columns &= source_field[col][row] == symbol
+            rows_win &= source_field[row][col] == symbol
+            columns_win &= source_field[col][row] == symbol
 
-        if rows or columns:
+        if rows_win or rows_win:
             return True
 
     return False
 
 
-def checking_status(source_field):
+def is_end_game(source_field):
     total_symbols = [symbol for row in source_field for symbol in row]
+    total_space = total_symbols.count(empty_symbol)
 
-    total_x = total_symbols.count('X')
-    total_o = total_symbols.count('O')
-    total_space = total_symbols.count('_')
+    line_x_win = check_col_row_win(player1, source_field)
+    line_o_win = check_col_row_win(player2, source_field)
 
-    if abs(total_x - total_o) > 1:
-        return "Impossible"
+    if check_diag_win(player1, source_field) or line_x_win:
+        print(f"{player1} wins")
+        return True
 
-    line_x_win = check_col_row_win('X', source_field)
-    line_o_win = check_col_row_win('O', source_field)
+    if check_diag_win(player2, source_field) or line_o_win:
+        print(f"{player2} wins")
+        return True
 
-    if line_o_win and line_x_win:
-        return "Impossible"
-
-    if check_diag_win('X', source_field) or line_x_win:
-        return "X wins"
-
-    if check_diag_win('O', source_field) or line_o_win:
-        return "O wins"
-
-    if total_space > 0:
-        return "Game not finished"
+    if total_space == 0:
+        print("Draw")
+        return True
     else:
-        return "Draw"
+        return False
 
 
 def get_coordinates(source_field):
@@ -76,41 +72,48 @@ def get_coordinates(source_field):
 
         is_not_digit = True
         while is_not_digit:
-            for i in user_turn:
-                if not i.isdigit():
-                    is_not_digit = True
-                    print("You should enter numbers!")
-                    user_turn = input("Enter the coordinates: ").split()
-                    break
-                else:
-                    is_not_digit = False
+            if not all(coord.isdigit() for coord in user_turn):
+                print("You should enter numbers!")
+                user_turn = input("Enter the coordinates: ").split()
+                break
+            else:
+                is_not_digit = False
 
         user_coord = [int(c) for c in user_turn]
 
         if len(user_coord) < 2:
             print("Error! Need 2 coordinates!")
-        elif user_coord[0] not in range(1, 4) or user_coord[1] not in range(1, 4):
+        elif not all(coord in range(1, 4) for coord in user_coord):
             print("Coordinates should be from 1 to 3!")
-        elif source_field[user_coord[0]-1][user_coord[1]-1] != '_':
+        elif source_field[user_coord[0]-1][user_coord[1]-1] != empty_symbol:
             print("This cell is occupied! Choose another one!")
         else:
             return user_coord
 
 
-def update_field(coordinates, source_field):
+def update_field(coordinates, source_field, player_symbol):
     row = coordinates[0] - 1
     col = coordinates[1] - 1
-    source_field[row][col] = 'X'
+    source_field[row][col] = player_symbol
     return source_field
 
 
+def player_turn(player_symbol, field):
+    player_coordinates = get_coordinates(field)
+    return update_field(player_coordinates, field, player_symbol)
+
+
 def main():
-    source = input("Enter cells: ")
-    field = init_cells(source)
+    player_symbol = player1
+    field = init_cells(empty_symbol)
     print_field(field)
-    coordinates = get_coordinates(field)
-    field = update_field(coordinates, field)
-    print_field(field)
+    while True:
+        field = player_turn(player_symbol, field)
+        print_field(field)
+        if is_end_game(field):
+            break
+        else:
+            player_symbol = player2 if player_symbol == player1 else player1
 
 
 if __name__ == '__main__':
