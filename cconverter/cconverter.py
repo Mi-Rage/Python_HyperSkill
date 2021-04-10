de859ca1be03f599e105d2b96eba5b1df1758718
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2021-04-10 14:09:11 +0300

    Currency Converter Stage 4/6
    Currency Converter Python project. Stage 4/6 complete

--- cconverter/cconverter.py
+++ cconverter/cconverter.py
@@ -1,7 +1,9 @@
 # write your code here!
 source_currency = "conicoins"
-destination_currency = "dollars"
-coins = int(input(f"Please, enter the number of {source_currency} you have: "))
-rate = float(input("Please, enter the exchange rate: "))
-exchange = coins * rate
-print(f"The total amount of {destination_currency}: {exchange}")
+destination_currency = {'RUB': 2.98, 'ARS': 0.82, 'HNL': 0.17, 'AUD': 1.9622, 'MAD': 0.208}
+
+coins = float(input())
+for key in destination_currency:
+    rate = round(coins * destination_currency[key], 2)
+    print(f"I will get {rate} {key} from the sale of {coins} conicoins.")
+
