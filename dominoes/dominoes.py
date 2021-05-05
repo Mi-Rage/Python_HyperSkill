b3b0c08e8b3406ca7b00fc2fd028fd098708d22a
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2021-05-05 10:18:20 +0300

    Dominoes Stage 2/5
    Dominoes Python project. Stage 2/5 complete

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -6,10 +6,5 @@ import game
 stock = stock.Stock()
 users = [user.User('Computer', stock), user.User('Player', stock)]
 game = game.Game(stock, users)
-first_move = game.get_first_move()
-
-print(f'Stock pieces: {stock.pieces}')
-for usr in users:
-    print(f'{usr.user_name} pieces: {usr.user_set}')
-print(f'Domino snake: {game.snake}')
-print(f'Status: {first_move.user_name.lower()}')
+game.get_first_move()
+game.print_situation()
--- dominoes/game.py
+++ dominoes/game.py
@@ -1,5 +1,6 @@
 class Game:
     snake = []
+    next_turn = None
 
     def __init__(self, stock, users):
         self.stock = stock
@@ -20,4 +21,25 @@ class Game:
                     index_dbl = doubles.index(dbl)
         self.users[index_dbl].user_set.remove(doubles[index_dbl])
         self.snake.append(doubles[index_dbl])
-        return self.users[(index_dbl + 1) % len(self.users)]
+        self.next_turn = self.users[(index_dbl + 1) % len(self.users)]
+
+    def print_snake(self):
+        for piece in self.snake:
+            print(piece)
+
+    def print_status(self):
+        if self.next_turn.user_name == "Player":
+            print("Status: It's your turn to make a move. Enter your command.")
+        else:
+            print("Status: Computer is about to make a move. Press Enter to continue...")
+
+    def print_situation(self):
+        print("=" * 70)
+        print(f"Stock size: {len(self.stock.pieces)}")
+        print(f"Computer pieces: {len(self.users[0].user_set)}\n")
+        self.print_snake()
+        print()
+        print("Your pieces:")
+        for index in range(len(self.users[1].user_set)):
+            print(f"{index + 1}: {self.users[1].user_set[index]}")
+        self.print_status()
