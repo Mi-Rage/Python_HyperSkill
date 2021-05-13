a548177c9129f1eae391c79cc2fae05989eade69
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2021-05-13 16:19:06 +0300

    Bill Splitter Stage 2/4
    Bill Splitter Python project. Stage 2/4 complete.

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,4 +1,4 @@
-friends_dict = dict()
+friends_list = list()
 
 print("Enter the number of friends joining (including you)")
 num_of_people = int(input())
@@ -8,6 +8,9 @@ if num_of_people <= 0:
 else:
     print("Enter the name of every friend (including you), each on a new line:")
     for _ in range(num_of_people):
-        name = input()
-        friends_dict[name] = 0
+        friends_list.append(input())
+    print("Enter the total bill value:")
+    total_bill = int(input())
+    each_bill = round(total_bill / num_of_people, 2)
+    friends_dict = {name: each_bill for name in friends_list}
     print(friends_dict)
