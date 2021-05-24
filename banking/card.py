9770a6e0232c4c28d343e159cdb511934c1bc398
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2021-05-24 21:11:08 +0300

    Simple Banking System Stage 2/4
    Simple Banking System Python project. Stage 2/4 complete.

--- banking/card.py
+++ banking/card.py
@@ -5,9 +5,9 @@ class Card:
 
     def __init__(self):
         self.iin = 400000
-        self.checksum = 5
         self.digit_in_acc_number = 9
         self.pin_digit = 4
+        self.check_sum = 0
         self.card_number = self.create_card_number()
         self.pin = self.create_pin()
 
@@ -15,10 +15,28 @@ class Card:
         acc_number = ""
         for n in range(self.digit_in_acc_number):
             acc_number += str(random.randint(0, 9))
-        return str(self.iin) + acc_number + str(self.checksum)
+        number_without_checksum = str(self.iin) + acc_number
+        check_sum = self.get_checksum(number_without_checksum)
+        return str(self.iin) + acc_number + str(check_sum)
 
     def create_pin(self):
         new_pin = ""
         for n in range(self.pin_digit):
             new_pin += str(random.randint(0, 9))
         return new_pin
+
+    def get_checksum(self, checked_card_number):
+        result = 0
+        for index in range(15):
+            item = int(checked_card_number[index])
+            if index % 2 == 0:
+                item = item * 2
+                if item > 9:
+                    item = item - 9
+            print(item)
+            result += item
+        if result % 10 != 0:
+            self.check_sum = 10 - result % 10
+        else:
+            self.check_sum = 0
+        return self.check_sum
