397106fad85dc40d608090bf7645358d88312e78
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2021-06-23 20:38:08 +0300

    Web Scraper Stage 2/5
    Web Scraper Python project. Stage 2/5 complete.

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,8 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
 url_path = input()
-r = requests.get(url_path)
-if r.status_code == 200 and 'content' in r.json().keys():
-    print(r.json()['content'])
-else:
-    print("Invalid quote resource!")
+r = requests.get(url_path, headers={'Accept-Language': 'en-US,en;q=0.5'})
+soup = BeautifulSoup(r.content, 'html.parser')
+result = {}
+try:
+    title = soup.find('title')
+    result['title'] = title.text
+    summary = soup.find('div', class_='summary_text').get_text()
+    result['description'] = summary.strip()
+    print(result)
+except (LookupError, AttributeError):
+    print("Invalid movie page!")
+
