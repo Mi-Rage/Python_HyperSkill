e9dda41e4a560244605f79eaf05c67461eb151c6
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2021-06-23 21:06:59 +0300

    Web Scraper Stage 3/5
    Web Scraper Python project. Stage 3/5 complete.

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,16 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
 
 url_path = input()
-r = requests.get(url_path, headers={'Accept-Language': 'en-US,en;q=0.5'})
-soup = BeautifulSoup(r.content, 'html.parser')
-result = {}
-try:
-    title = soup.find('title')
-    result['title'] = title.text
-    summary = soup.find('div', class_='summary_text').get_text()
-    result['description'] = summary.strip()
-    print(result)
-except (LookupError, AttributeError):
-    print("Invalid movie page!")
-
+r = requests.get(url_path)
+status = r.status_code
+if status == 200:
+    content = requests.get(url_path).content
+    with open('source.html', 'wb') as file:
+        file.write(content)
+    print("Content saved.")
+else:
+    print("The URL returned ", status)
