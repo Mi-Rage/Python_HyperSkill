2c6dd88c4f4309d0e326b1fd5a66089c51bd84ad
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2021-06-29 20:44:14 +0300

    Web Scraper Stage 4/5
    Web Scraper Python project. Stage 4/5 complete.

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,12 +1,33 @@
 import requests
+import string
+from bs4 import BeautifulSoup
 
-url_path = input()
-r = requests.get(url_path)
-status = r.status_code
-if status == 200:
-    content = requests.get(url_path).content
-    with open('source.html', 'wb') as file:
-        file.write(content)
-    print("Content saved.")
-else:
-    print("The URL returned ", status)
+url_path = "https://www.nature.com/nature/articles"
+response = requests.get(url_path)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+articles = soup.find_all('article')
+for article in articles:
+    category = article.find('span', {'class': 'c-meta__type'}).text
+    if category.lower() == 'news':
+        print("----------------------")
+        print(category)
+
+        name = article.find('a').text
+        for n in name:
+            if n in string.punctuation:
+                name = name.replace(n, '')
+        name = name.replace(' ', '_')
+        print(name)
+
+        href = 'https://www.nature.com' + article.find('a', {'class': 'c-card__link u-link-inherit'}).get('href')
+        print(href)
+
+        article_response = requests.get(href)
+        article_soup = BeautifulSoup(article_response.content, 'html.parser')
+        article_div = article_soup.find('div', {'class': 'c-article-body u-clearfix'}).text
+        article_text = article_div.replace('\n', '')
+        print(article_text)
+
+        with open(name + ".txt", 'wb') as file:
+            file.write(article_text.encode())
