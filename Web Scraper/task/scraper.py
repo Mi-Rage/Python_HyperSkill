d172955c529c8e4a656e917a5c336828caa8643a
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2021-06-30 15:58:12 +0300

    Web Scraper Stage 5/5
    Web Scraper Python project. Stage 5/5 complete.

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,33 +1,57 @@
 import requests
 import string
+import os
 from bs4 import BeautifulSoup
 
-url_path = "https://www.nature.com/nature/articles"
-response = requests.get(url_path)
-soup = BeautifulSoup(response.content, 'html.parser')
+pages = int(input())
+types_articles = input()
 
-articles = soup.find_all('article')
-for article in articles:
-    category = article.find('span', {'class': 'c-meta__type'}).text
-    if category.lower() == 'news':
-        print("----------------------")
-        print(category)
+current_directory = os.getcwd()
 
-        name = article.find('a').text
-        for n in name:
-            if n in string.punctuation:
-                name = name.replace(n, '')
-        name = name.replace(' ', '_')
-        print(name)
+for page in range(pages):
+    directory_to_save = 'Page_' + str(page + 1)
+    os.mkdir(directory_to_save)
+    os.chdir(directory_to_save)
 
-        href = 'https://www.nature.com' + article.find('a', {'class': 'c-card__link u-link-inherit'}).get('href')
-        print(href)
+    url_path = "https://www.nature.com/nature/articles"
+    parameter = {'searchType': 'journalSearch', 'sort': 'PubDate', 'page': page + 1}
+    response = requests.get(url_path, params=parameter)
+    print(response.url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-        article_response = requests.get(href)
-        article_soup = BeautifulSoup(article_response.content, 'html.parser')
-        article_div = article_soup.find('div', {'class': 'c-article-body u-clearfix'}).text
-        article_text = article_div.replace('\n', '')
-        print(article_text)
+    articles = soup.find_all('article')
 
-        with open(name + ".txt", 'wb') as file:
-            file.write(article_text.encode())
+    for article in articles:
+        category = article.find('span', {'class': 'c-meta__type'}).text
+        if category == types_articles:
+            print("----------------------")
+            print(category)
+
+            name = article.find('a').text
+            for n in name:
+                if n in string.punctuation:
+                    name = name.replace(n, '')
+            name = name.replace(' ', '_')
+            print(name)
+
+            href = 'https://www.nature.com' + article.find('a', {'class': 'c-card__link u-link-inherit'}).get(
+                'href')
+            print(href)
+
+            article_response = requests.get(href)
+            article_soup = BeautifulSoup(article_response.content, 'html.parser')
+            article_div = ""
+            try:
+                article_div = article_soup.find('div', {'class': 'article-item__body'}).text
+            except AttributeError:
+                try:
+                    article_div = article_soup.find('div', {'class': 'c-article-body u-clearfix'}).text
+                except AttributeError:
+                    print("WOW! Attribute error")
+
+            article_text = article_div.replace('\n', '')
+            print(article_text)
+
+            with open(name + ".txt", 'wb') as file:
+                file.write(article_text.encode())
+    os.chdir(current_directory)
